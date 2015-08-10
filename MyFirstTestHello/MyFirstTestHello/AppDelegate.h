1affaaa1e7aab92f5cb77e4c82c4c7e7804c2432
miaofangwang <miaofangwang@miaofangwangdeiMac.local>
2015-08-10 16:33:57 +0800

    8.10.1

---
+++ MyFirstTestHello/MyFirstTestHello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstTestHello
+//
+//  Created by miaofangwang on 15/8/10.
+//  Copyright (c) 2015年 miaofangwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFirstTestHello/MyFirstTestHello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstTestHello
+//
+//  Created by miaofangwang on 15/8/10.
+//  Copyright (c) 2015年 miaofangwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
